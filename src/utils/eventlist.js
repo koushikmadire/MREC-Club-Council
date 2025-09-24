48e21b2b04dec0c5c9dfd8d07f937b0222f48d86
koushikmadire <koushikmadire039@gmail.com>
2025-09-24 12:07:09 +0530

    deployed

--- src/utils/eventlist.js
+++ src/utils/eventlist.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
 const ClubCouncilList = [
     {
-        id: uuidv4(),
+        // id: uuidv4(),
         // imgUrl:"https://www.shutterstock.com/image-photo/portrait-handsome-caucasian-man-formal-260nw-2142820441.jpg",
         imgUrl: "https://www.instagram.com/p/DOxD-4cD1M6/?igsh=aWJmMHpwMWFhNjdo",
         name: "VISHESH 2K25",
@@ -9,7 +9,7 @@ const ClubCouncilList = [
 
     },
     {
-        id: uuidv4(),
+        // id: uuidv4(),
         // imgUrl:"https://www.shutterstock.com/image-photo/portrait-handsome-caucasian-man-formal-260nw-2142820441.jpg",
         imgUrl: "https://clubs.iiit.ac.in/_next/image?url=http%3A%2F%2Ffiles%2Ffiles%2Fdownload%3Ffilename%3DingBq3cwFzUyxdd2fGTWGH_Outlook-gypacaqa.png&w=2048&q=75",
         name: "Intro to Cryptography",
